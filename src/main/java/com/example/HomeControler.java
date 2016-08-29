ccb0185aec03d932934576b7eb7b4a34a6ab66af
arthurn1910 <radekpawlaczyk@gmail.com>
2016-08-29 20:29:43 +0200

    authentication without user from database

--- src/main/java/com/example/HomeControler.java
+++ src/main/java/com/example/HomeControler.java
@@ -1,15 +1,31 @@
 package com.example;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.Date;
+import java.util.Map;
 
 /**
  * Created by Medion on 2016-08-28.
  */
-@RestController
-public class HomeControler {
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+
+@SpringBootApplication
+@Controller
+public class HomeControler{
+    public static void main(String[] args) throws Exception {
+        new SpringApplicationBuilder(HomeControler.class).run(args);
     }
 }
